18991f7fa855481d37d00e829e235978280a914a
Luis Facunde <luisfacunde19@gmail.com>
2024-11-05 21:24:40 -0300

    Adição mecânica de movimento e spawn de frutas

--- src/main.c
+++ src/main.c
@@ -6,15 +6,17 @@
 */
 
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
 
-int x = 34, y = 12; // Posição inicial da nave
-int incX = 1, incY = 1; // Incrementos para movimentação da nave
+int x = MAXX/2, y = MAXY/2; // Posição inicial da nave
+int incX = 1, incY = 0; // Incrementos para movimentação da nave
 
-void desenhaNave(int nextX, int nextY) {
+void desenhaCobra(int nextX, int nextY) {
     screenSetColor(CYAN, DARKGRAY);
     
     screenGotoxy(x, y);         
@@ -46,28 +48,43 @@ void printKey(int ch) {
     }
 }
 
+void spawn_object() {
+    int objX = rand() % (MAXX - 5) + 1; // Garante que o objeto esteja dentro dos limites
+    int objY = rand() % (MAXY - 5) + 1;
+
+    screenSetColor(RED, DARKGRAY);
+    screenGotoxy(objX, objY);
+    printf("*"); 
+}
+
 int main() {
     static int ch = 0;
+    srand(time(0));
 
     screenInit(1);
     keyboardInit();
-    timerInit(1000);
+    timerInit(100);
 
-    desenhaNave(x, y);
+    desenhaCobra(x, y);
     screenUpdate();
 
-    while (ch != 10) { // Sai do loop quando a tecla Enter (código 10) é pressionada
-        // Lida com a entrada do usuário
+    int spawn_counter = 0;
+
+    while (ch != 10) {
         if (keyhit()) {
             ch = readch();
-            if (ch == 'd'){
-                desenhaNave(x + 2, y);
-            } else if (ch == 'a'){
-                desenhaNave(x - 2, y);
-            } else if (ch == 's'){
-                desenhaNave(x, y + 2);
-            } else if (ch == 'w'){
-                desenhaNave(x, y - 2);
+            if (ch == 'd') {
+                incX = 1;
+                incY = 0;
+            } else if (ch == 'a') {
+                incX = -1;
+                incY = 0;
+            } else if (ch == 's') {
+                incX = 0;
+                incY = 1;
+            } else if (ch == 'w') {
+                incX = 0;
+                incY = -1;
             }
             printKey(ch);
             screenUpdate();
@@ -77,78 +94,28 @@ int main() {
             int newX = x + incX;
             int newY = y + incY;
 
-            if (newX >= (MAXX - 1) || newX <= MINX + 1) incX = -incX;
-            if (newY >= MAXY - 1 || newY <= MINY + 1) incY = -incY;
+            if (newX >= (MAXX - 2) || newX <= MINX + 1){
+                incX = -incX;
+            }
+            if (newY >= (MAXY - 1) || newY <= (MINY + 1)){
+                incY = -incY;
+            }
 
-            desenhaNave(newX, newY);
+            desenhaCobra(newX, newY);
             screenUpdate();
+
+            spawn_counter++;
+
+            if (spawn_counter >= 35) {
+                spawn_object();
+                spawn_counter = 0; // Reseta o contador
+            }
         }
     }
 
-    // Finaliza as bibliotecas
     keyboardDestroy();
     screenDestroy();
     timerDestroy();
 
     return 0;
 }
-
-
-
-// #include <SDL2/SDL.h>
-// #include <stdio.h>
-
-// int main(int argc, char *argv[]) {
-//     // Initialize SDL
-//     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-//         printf("Error initializing SDL: %s\n", SDL_GetError());
-//         return -1;
-//     }
-
-//     // Create a window
-//     SDL_Window *window = SDL_CreateWindow(
-//         "Simple Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN
-//     );
-//     if (!window) {
-//         printf("Error creating window: %s\n", SDL_GetError());
-//         return -1;
-//     }
-
-//     // Create a renderer
-//     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-//     if (!renderer) {
-//         printf("Error creating renderer: %s\n", SDL_GetError());
-//         SDL_DestroyWindow(window);
-//         return -1;
-//     }
-
-//     // Game loop
-//     int running = 1;
-//     SDL_Event event;
-
-//     while (running) {
-//         // Handle events
-//         while (SDL_PollEvent(&event)) {
-//             if (event.type == SDL_QUIT) {
-//                 running = 0;
-//             }
-//         }
-
-//         // Clear the screen with a color
-//         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-//         SDL_RenderClear(renderer);
-
-//         // Present the screen
-//         SDL_RenderPresent(renderer);
-
-//         // Delay to control frame rate
-//         SDL_Delay(16); // Roughly 60 FPS
-//     }
-
-//     // Cleanup
-//     SDL_DestroyRenderer(renderer);
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
-
-//     return 0;
-// }
